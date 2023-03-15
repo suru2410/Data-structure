4e3daeb0368b255486b7aaf913368b3a251814a6
suru2410 <121425651+suru2410@users.noreply.github.com>
2023-03-15 09:43:14 +0530

    Update tree traversal 11.c

--- tree traversal 11.c
+++ tree traversal 11.c
@@ -1,69 +1,77 @@
-//stack using linked list
-#include<stdio.h>
-struct Node
-{
-   int data;
-   struct Node *next;
-}*top = NULL;
+// Tree traversal in C
 
-void push(int);
-void pop();
-void display();
+#include <stdio.h>
+#include <stdlib.h>
 
-void main()
-{
-   int choice, value;
+struct node {
+  int item;
+  struct node* left;
+  struct node* right;
+};
 
-   while(1){
-      printf("1. Push\n2. Pop\n3. Display\n4. Exit\n");
-      printf("Enter your choice: ");
-      scanf("%d",&choice);
-      switch(choice){
-	 case 1: printf("Enter the value to be insert: ");
-		 scanf("%d", &value);
-		 push(value);
-		 break;
-	 case 2: pop(); break;
-	 case 3: display(); break;
-	 case 4: exit(0);
-	 default: printf("\nWrong choice\n");
-      }
-   }
-}
-void push(int value)
-{
-   struct Node *newNode;
-   newNode = (struct Node*)malloc(sizeof(struct Node));
-   newNode->data = value;
-   if(top == NULL)
-      newNode->next = NULL;
-   else
-      newNode->next = top;
-   top = newNode;
-   printf("\nElement is pushed \n");
-}
-void pop()
-{
-   if(top == NULL)
-      printf("\nStack is Empty\n");
-   else{
-      struct Node *temp = top;
-      printf("\nDeleted element: %d\n", temp->data);
-      top = temp->next;
-      free(temp);
-   }
-}
-void display()
-{
-   if(top == NULL)
-      printf("\nStack is Empty\n");
-   else{
-      struct Node *temp = top;
-      while(temp->next != NULL){
-	 printf("%d\n",temp->data);
-	 temp = temp -> next;
-      }
-      printf("%d\n",temp->data);
-   }
+// Inorder traversal
+void inorderTraversal(struct node* root) {
+  if (root == NULL)
+  return;
+  inorderTraversal(root->left);
+  printf("%d ->", root->item);
+  inorderTraversal(root->right);
 }
 
+// preorderTraversal traversal
+void preorderTraversal(struct node* root) {
+  if (root == NULL)
+  return;
+  printf("%d ->", root->item);
+  preorderTraversal(root->left);
+  preorderTraversal(root->right);
+}
+
+// postorderTraversal traversal
+void postorderTraversal(struct node* root) {
+  if (root == NULL)
+  return;
+  postorderTraversal(root->left);
+  postorderTraversal(root->right);
+  printf("%d ->", root->item);
+}
+
+// Create a new Node
+struct node* createNode(value) {
+  struct node* newNode = malloc(sizeof(struct node));
+  newNode->item = value;
+  newNode->left = NULL;
+  newNode->right = NULL;
+
+  return newNode;
+}
+
+// Insert on the left of the node
+struct node* insertLeft(struct node* root, int value) {
+  root->left = createNode(value);
+  return root->left;
+}
+
+// Insert on the right of the node
+struct node* insertRight(struct node* root, int value) {
+  root->right = createNode(value);
+  return root->right;
+}
+
+int main() {
+  struct node* root = createNode(1);
+  insertLeft(root, 2);
+  insertRight(root, 3);
+
+  insertLeft(root->left, 4);
+  insertRight(root->left, 5);
+
+  printf("Inorder traversal \n");
+  inorderTraversal(root);
+
+  printf("\nPreorder traversal \n");
+  preorderTraversal(root);
+
+  printf("\nPostorder traversal \n");
+  postorderTraversal(root);
+}
